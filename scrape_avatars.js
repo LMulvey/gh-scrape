d2bc2d8dae9cdbc4476b21acb5746984f142f275
Lee Mulvey <lmulvey@me.com>
2017-07-24 02:30:53 +0000

    Working project.

--- scrape_avatars.js
+++ scrape_avatars.js
@@ -4,7 +4,7 @@ const request = require('request'),
 // * Define constants //
 const GITHUB_USER = "LMulvey";
 const GITHUB_TOKEN = "58feac3c8a547b9cc4d423805605e1c87804ddb8";
-
+const USER_AGENT = { 'User-Agent': 'LMulvey' }
 console.log('Welcome to GH Scrape.\n -> Let\'s scrape some avatars!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -14,7 +14,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
     + repoOwner + "/" + repoName + 
     "/contributors";
 
-    request.get({ url: request_url, headers: { 'User-Agent': 'LMulvey' }},
+    request.get({ url: request_url, headers: USER_AGENT },
      (err, res, body) => {
         if(err) cb(err);
         else cb(null, res, JSON.parse(body));
@@ -22,13 +22,23 @@ function getRepoContributors(repoOwner, repoName, cb) {
 }
 
 function downloadImageByURL(url, filePath) {
-
-    
+    request.get({ url: url, headers: USER_AGENT })
+    .on('err', (err) => {
+        return err;
+    })
+    .on('response', (response) => {
+        console.log('Downloading file...\n');
+        console.log('Received repsonse code: ' + response.statusCode);
+    })
+    .pipe(fs.createWriteStream(filePath));
 }
 
 getRepoContributors("jquery", "jquery", (err, res, body) => {
     console.log("Errors: " + err +
     "\nResult: ");
-    for (var key in body) console.log(body[key]['avatar_url']);
+    for (var key in body) {
+        downloadImageByURL(body[key]['avatar_url'], './avatars/' 
+        + body[key]['login'] + '.jpg');
+    };
 });
 
