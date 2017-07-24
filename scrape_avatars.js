cc92e57e4a3b0b3f5446f312eaf8debfb69e52a8
Lee Mulvey <lmulvey@me.com>
2017-07-24 03:31:54 +0000

    Added additional functionality.

--- scrape_avatars.js
+++ scrape_avatars.js
@@ -2,13 +2,17 @@ const request = require('request'),
         fs = require('fs');
 
 // * Define constants //
-const GITHUB_USER = "LMulvey";
-const GITHUB_TOKEN = "58feac3c8a547b9cc4d423805605e1c87804ddb8";
-const USER_AGENT = { 'User-Agent': 'LMulvey' }
+const GITHUB_USER = "LMulvey",
+    GITHUB_TOKEN = "58feac3c8a547b9cc4d423805605e1c87804ddb8",
+    USER_AGENT = { 'User-Agent': 'LMulvey' },
+    scrapeUser = (!process.argv[2]) ? "LMulvey" : process.argv[2],
+    scrapeRepo = (!process.argv[3]) ? "frotos" : process.argv[3];
+
+//Intro
 console.log('Welcome to GH Scrape.\n -> Let\'s scrape some avatars!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    let request_url = "https://"
+    const request_url = "https://"
      + GITHUB_USER + ":" + GITHUB_TOKEN + 
     "@api.github.com/repos/" 
     + repoOwner + "/" + repoName + 
@@ -27,18 +31,25 @@ function downloadImageByURL(url, filePath) {
         return err;
     })
     .on('response', (response) => {
-        console.log('Downloading file...\n');
+        console.log('Downloading file ' + url + ' to path: ' + filePath);
         console.log('Received repsonse code: ' + response.statusCode);
     })
+    .on('end',  () => { 
+        console.log('Download completed.');
+    })
     .pipe(fs.createWriteStream(filePath));
+   
 }
 
-getRepoContributors(process.argv[2], process.argv[3], (err, res, body) => {
+getRepoContributors(scrapeUser, scrapeRepo, (err, res, body) => { 
     console.log("Errors: " + err +
     "\nResult: ");
-    for (var key in body) {
+    console.log('*********** STARTING DOWNLOAD OF ' + body.length + ' FILES... *************');
+    for (let key in body) {
         downloadImageByURL(body[key]['avatar_url'], './avatars/' 
         + body[key]['login'] + '.jpg');
     };
+    console.log('*********** DOWNLOADED ' + body.length + ' FILES... *************');
+    console.log('*********** CHECK ./AVATARS/ FOLDER FOR RESULTS *************');
 });
 
